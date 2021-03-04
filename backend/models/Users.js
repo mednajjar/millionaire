fa0533b18ce50146bb71bcef1a9455c7c39f4259
Mohammed Najjar <najjarmedd@gmail.com>
2021-03-04 13:15:47 +0100

    update user model

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -4,9 +4,20 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     userName: { type: String, required: true},
     phone: { type: String, required: true},
-    email: { type: String, required: true},
-    role: { type: String, required: true},
-    status: { type: Number, required: true},
+    email: { 
+        type: String,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+        required: true
+        },
+    password: { type: String, required: true},
+    roles: {
+        type: [{
+            type: String,
+            enum: ['user', 'admin']
+        }],
+        default: ['user'],
+    },
+    status: { type: Boolean, default:false},
 })
 
 module.exports = mongoose.model('User', userSchema)
